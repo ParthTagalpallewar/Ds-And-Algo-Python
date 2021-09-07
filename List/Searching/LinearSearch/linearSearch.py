bb4b5a59832c5c040e79514db708639e9826c1a2
Parth Tagalpallewar <parthtagalpallewar123@gamil.com>
2021-09-07 22:32:05 +0530

    Adding Linear Search Program

--- List/Searching/LinearSearch/linearSearch.py
+++ List/Searching/LinearSearch/linearSearch.py
@@ -1,1 +1,25 @@
-print("hello world")
+
+"""
+Time Complexity For Linear Search 
+
+Best Case : O(1) 
+Worst Case : O(n)
+
+"""
+
+
+def linearSearch(list, n):
+
+    for i in range(0, len(list)):
+        if(list[i] == n):
+            return i
+    else:
+        return -1
+
+list = [100, 300, 800]
+numberToSearch = 30
+
+#Returning Index of Number In List
+indexOfNumber = linearSearch(list, numberToSearch)
+
+print("index is ", indexOfNumber) if(indexOfNumber != -1) else print("Number Not found")
