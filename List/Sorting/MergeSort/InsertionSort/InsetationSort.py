7543aaacc23b28b783849be793379ba195e05eeb
Parth Tagalpallewar <parthtagalpallewar123@gamil.com>
2021-09-17 09:32:22 +0530

    Insertation Sort

---
+++ List/Sorting/MergeSort/InsertionSort/InsetationSort.py
@@ -0,0 +1,17 @@
+def insertationSort(list):
+
+    for i in range(1, len(list)):
+        j = i-1
+        
+        while(j >= 0 and list[j] > list[i]):
+            
+            list[i], list[j] = list[j], list[i]
+          
+            j -= 1
+            i -= 1
+          
+
+    return list
+
+list = [5, 4, 3, 2, 1]
+print(insertationSort(list))
