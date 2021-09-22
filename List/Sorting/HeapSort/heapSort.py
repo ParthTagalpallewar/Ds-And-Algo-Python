481f51209869bdbd2d48a0afa3fb0ca84a5f3e8d
Parth Tagalpallewar <parthtagalpallewar123@gamil.com>
2021-09-22 12:36:00 +0530

    Heap Sort Completed

---
+++ List/Sorting/HeapSort/heapSort.py
@@ -0,0 +1,32 @@
+def heapify(list, size, i):
+    largest = i
+    left = (2 * i) + 1
+    right = (2 * i) + 2
+
+    if(left < size and list[left] > list[largest]):
+        largest = left
+
+    if(right < size and list[right] > list[largest]):
+        largest = right
+
+    if(i != largest):
+        #swap i with largest
+        list[i], list[largest] = list[largest], list[i] 
+        heapify(list, size, largest)
+
+def heapSort(list):
+    n = len(list)
+
+    #Building Max tree
+    for i in range(n//2, -1, -1):
+        heapify(list, n, i)
+
+    for i in range(n-1, 0, -1):
+
+        list[i], list[0] = list[0], list[i]
+
+        heapify(list, i, 0)
+
+arr = [1, 12, 9, 5, 6, 10]
+heapSort(arr)
+print(arr)
