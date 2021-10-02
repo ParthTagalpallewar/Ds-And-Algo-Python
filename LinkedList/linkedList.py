14b9bdd0d64acfba3356c5472597769e19c53864
Parth Tagalpallewar <parthtagalpallewar123@gamil.com>
2021-10-02 15:30:23 +0530

    remove dublicates in linkedlist

--- LinkedList/linkedList.py
+++ LinkedList/linkedList.py
@@ -76,32 +76,99 @@ class LinkedList:
 
         temp.next = temp.next.next
         
-    def insetElements(self, data):
+    def insertElements(self, data):
 
         for _ in data:
             self.insertAtEnd(_)
         
+    def getNthFromLast(self, position):
+        dummy = Node(0, self.head)
+
+        fast = dummy
+        slow = dummy
+
+        counter = 0
+        while(fast and counter != position):
+            fast = fast.next
+            counter += 1
+
+      
+        while(fast.next):
+            slow = slow.next
+            fast = fast.next
+            
+        print(slow.data)
+
+    # [1, 1, 2, 3] -> [2, 3]
+    def removeAllDuplicates(self):
+
+        #[0, 1, 1, 1, 1, 2, 3, 4, 4, 4, 5, 6]
+
+        prev = Node(None, self.head)
+        
+        head = prev # head = 0
+    
+        start = prev.next # start = #[1, 1, 1, 1, 2, 3, 4, 4, 4, 5, 6]
+        selected = None
+
+        while(start.next): # 1 -> 1
+            if(start.data == start.next.data): # 1 == 1
+                selected = start.data # selected = 1
+                
+                while(start.next and start.next.data == selected): 
+                    start = start.next # start -> 1 position 4
+                
+                if(start.next): 
+                    prev.next = start.next 
+                    prev = start
+                    start = start.next
+                else:
+                    prev.next = None
+                    break
+
+                
+            else:
+                prev = start
+                start = prev.next
+                
+                
+            
+                
+        self.head = head.next    
+
+    def removeDuplicates(self):
+
+        # 0 0 0 0 0 0 0
+   
+        prevNode = self.head
+        nextNode = prevNode.next 
+
+        while(nextNode):
+
+            if(prevNode.data == nextNode.data):
+                while(nextNode and prevNode.data == nextNode.data):
+                        nextNode = nextNode.next 
+                prevNode.next = nextNode
+            else:
+                prevNode = prevNode.next 
+                nextNode = prevNode.next
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
 
     
     list = LinkedList() #head is null
 
-    list.insertAtEnd(0)
-    list.insertAtEnd(1)
+    list.insertElements("12234")
     
     list.print()
 
-    list.removeAt(1)
-
-    list.print()
-
-    list.insertAtEnd(100)
-    
-    list.print()
-
-    list.insetElements([1, 2, 3, 4, 5])
-
-    list.insertAt2(1, 999)
+    list.removeDuplicates()
 
     list.print()
