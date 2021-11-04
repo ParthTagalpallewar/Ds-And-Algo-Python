2ffa16fb2ce5d42ac3978e0546e48d194a1e96ab
Parth Tagalpallewar <parthtagalpallewar123@gamil.com>
2021-11-04 15:31:00 +0530

    Solved Minimum Steps to Reach End of Array Problem

--- List/Practise_Questions/PossibleCombinations.py
+++
@@ -1,8 +0,0 @@
-def combo(line):
-    b =[]
-    one_line = list(line)
-    for i in range(len(line)):
-        for i in range(len(line)-1):
-            one_line[i], one_line[i + 1] = one_line[i + 1] , one_line[i]
-            b.append("".join(one_line))
-    return b
--- List/Practise_Questions/SwapFirstAndLastElement.py
+++
@@ -1,9 +0,0 @@
-list = [1,2,3,4,5,67]
-
-list[0], list[-1] = list[-1], list[0]
-
-print(list)
-
-get = 1, 2
-
-print(type(get))
